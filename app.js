8094c7d4d061c2ca6325fe821e8704b50c4c9967
Joy Kirii <joykirii@gmail.com>
2023-04-18 22:09:32 +0100

    add styling to pages

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ const app = express();
 //Imports connection.js module - database connection
 const connection = require("./connection");
 
+//hash passwords
+const bcrypt = require('bcrypt');
+
+
+
 app.set("view engine", "ejs");
+
+app.use(express.json());
+
+//access the css/images
 app.use(express.static(__dirname + "/public"));
+
+//parse the form data
+app.use(express.urlencoded({extended: true}));
 
 //index 
 app.get("/", (req, res) => {
@@ -54,8 +66,6 @@ app.get("/collections", (req, res) => {
 
 //single collection
 app.get("/collection",(req,res)=>{
-
-  const collectionId = req.params.collectionId;
   res.render("collection");
   
 });
@@ -67,11 +77,63 @@ app.get("/sign-up",(req,res) =>{
 
 //Login
 app.get("/login",(req,res) =>{
+  // let usernameOrEmail = req.body.usernameOrEmail;
+  // let password = req.body.password;
+
+  // let checkuser = 'SELECT * FROM users WHERE (email = ? OR username = ?) AND password = ?';
+
+  // connection.query(checkuser, [usernameOrEmail],[password],(err,results) =>{
+  //   if(err) throw (err);
+  //   let numResults = results.length;
+  //   if(numResults > 0){
+  //     res.send('<code>logged in</code>');
+  //   }else{
+  //     res.send('<code>access denied</code>');
+  //   }
+  // });
   res.render("login");
 });
+
+//check login
+app.post("/login", (req, res) => {
+  const usernameOrEmail = req.body.usernameOrEmail;
+  const password = req.body.password;
+
+  const query = "SELECT * FROM users WHERE email = ? OR username = ?";
+
+  connection.query(query, [usernameOrEmail, usernameOrEmail], (err, rows, fields) => {
+    if (err) {
+      console.error(err);
+    }
+
+    if (rows.length > 0) {
+      const user = rows[0];
+
+      // Compare the provided password with the stored hash using bcrypt
+      bcrypt.compare(password, user.password, (err, match) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ message: "Internal server error" });
+          return;
+        }
+        
+        if (match) {
+          res.status(200).json({ message: "Authentication successful" });
+        } else {
+          res.status(401).json({ message: "Incorrect password" });
+        }
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  });
+});
+
 
 //Start web server - listen to incoming requests on the specified port
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running at port 3000");
 });
+
+
 
