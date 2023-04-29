b14284af7f2ff559fcc0aa6340578d0c0f9615e7
Joy Kirii <joykirii@gmail.com>
2023-04-29 20:56:17 +0100

    created logic to add tracks

--- routes/addTrackToVinyl.js
+++ routes/addTrackToVinyl.js
@@ -16,7 +16,27 @@ router.get('/vinyls/:vinylId/add-tracks',(req,res)=>{
     const vinylId = req.params.vinylId;
     const userId = req.session.user.id;
 
-    res.render("addTrackToVinyl", { vinylId, session: req.session });
+    console.log("vinylId: ", vinylId);
+    console.log("userId: ", userId);
+
+    const query = `SELECT user_id FROM vinyl WHERE vinyl_id = ?;`;
+
+    //check to make sure that the user who wants to add vinyl created the vinyl
+    connection.query(query, [vinylId], (err,results)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(results.length && results[0].user_id === userId){
+                 res.render("addTrackToVinyl", {
+                   vinylId,
+                   session: req.session,
+                 });
+            }else{
+                res.send('Unauthorized');
+            }
+        }
+    });
+   
     
 });
 
@@ -25,16 +45,38 @@ router.get('/vinyls/:vinylId/add-tracks',(req,res)=>{
 router.post('/vinyls/:vinylId/add-tracks',(req,res)=>{
     const vinylId  = req.params.vinylId;
 
-    const query = `INSERT INTO track(track_name, vinyl_id,track_number,track_duration) VALUES(?,?,?,?);`;
+    const trackName = req.body.trackName;
+    const trackNumber =req.body.trackNumber;
+    const trackDuration = req.body.trackDuration;
+
+    if(Array.isArray(trackName) && Array.isArray(trackNumber) && Array.isArray(trackDuration)){
+        const tracks = trackName.map((name, index) =>{
+            return{
+                trackName: name,
+                trackNumber: trackNumber[index],
+                trackDuration: trackDuration[index],
+            }
+        });
 
 
-    connection.query(query,[vinylId],(err,results)=>{
-        if(err){
-            console.log(err);
-        }else{
-            res.redirect(`/vinyls/${vinylID}`);
-        }
-    });
+         console.log("Tracks:", tracks);
+         tracks.forEach((track) => {
+           const query = `INSERT INTO track(track_name, vinyl_id,track_number,track_duration) VALUES(?,?,?,?);`;
+
+           connection.query(
+             query,
+             [track.trackName, vinylId, track.trackNumber, track.trackDuration],
+             (err, results) => {
+               if (err) {
+                 console.log(err);
+               } else {
+                 res.redirect(`/vinyls/${vinylId}`);
+               }
+             }
+           );
+         });
+    }
+   
 });
 
 module.exports = router;
